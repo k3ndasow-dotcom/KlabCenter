73a10dfdf7c2476817754db548529fc5adc04e39
k3ndasow-dotcom <k3ndasow@gmail.com>
2026-08-14 19:23:35 +0000

    Add files via upload
    Mise à jour du service worker et des commandes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klabcenter-v6';
+const CACHE_NAME = 'klabcenter-v7';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
