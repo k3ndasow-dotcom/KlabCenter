2a7fd5cf99a36f48d91ac0a03e5cf8a5ae5142c1
k3ndasow-dotcom <k3ndasow@gmail.com>
2026-08-17 11:40:09 +0000

    Add files via upload
    Finalité

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'klabcenter-v20';
+const CACHE_NAME = 'klabcenter-v21';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-klabcenter.png'];
 
 self.addEventListener('install', event => {
